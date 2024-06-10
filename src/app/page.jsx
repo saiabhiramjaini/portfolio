05ddca5477ec26ee2b131c8b9ccbbb0494835bec
saiabhiramjaini <abhiramjaini28@gmail.com>
2024-06-11 00:18:10 +0530

    added profile

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -25,7 +25,7 @@ const Homepage = () => {
         animate={controls}
         transition={{ duration: 1 }}
       >
-        <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+        <div className="h-full flex flex-col gap-24  lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
           {/* IMAGE CONTAINER */}
           <div className="h-1/2 lg:h-full lg:w-1/2 relative">
             <Image src="/hero.png" alt="" fill className="object-contain" />
